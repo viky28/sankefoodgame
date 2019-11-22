9183222d3565f95197885e084910b144ac9a9997
viky28 <viky.pu28@gmail.com>
2019-11-22 16:43:05 +0530

    done start stop and restart

--- js/play.js
+++ js/play.js
@@ -1,16 +1,154 @@
-function demoX(x,y){
-	ctx.fillStyle = "red";
-    ctx.fillRect(x * snakeW, y * snakeH, canvas.width, snakeH);
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var snakeW = 10;
+var snakeH = 10;
+var dir = "right";
+let flag = true;
+var intervalVar;
+var newHead;
+var len = 4;
+var snake = [];
+var food = {
+    x: Math.round(Math.random() * (canvas.width / snakeW - 1) + 1),
+    y: Math.round(Math.random() * (canvas.height / snakeH - 1) + 1)
 }
-for(var i=0;i<=50;i++){
-	i++
-	demoX(0,i)
+
+function drawSnake(x, y) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+    ctx.fillStyle = 'black';
+    ctx.strokeRect(x * snakeW, y * snakeH, snakeW, snakeH)
 }
-function demoY(x,y){
-	ctx.fillStyle = "red";
-    ctx.fillRect(x * snakeW, y * snakeH, 10,canvas.height);
+//CreateSanke
+for (let i = len - 1; i >= 0; i--) {
+    snake.push({ x: i, y: 0 })
 }
-for(var i=0;i<=50;i++){
-	i++
-	demoY(i,0)
+
+//Direction and controls
+document.addEventListener("keydown", dirControl)
+
+function dirControl(e) {
+    console.log(e.key)
+    if (e.which == 32) {
+        if (flag) {
+            console.log("Start the game")
+            intervalVar = setInterval(drawDefaultSnake, 150);
+            flag = false;
+        } else {
+            console.log("Pause the game")
+            clearInterval(intervalVar);
+            flag = true;
+        }
+    }
+    if (e.which == 13){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        flag = true;
+    }
+    if (e.key == "ArrowRight" && dir != "left") {
+        dir = "right";
+    } else if (e.key === "ArrowLeft" && dir != "right") {
+        dir = "left"
+    } else if (e.key === "ArrowUp" && dir != "down") {
+        dir = "up"
+    } else if (e.key === "ArrowDown" && dir != "up") {
+        dir = "down"
+    }
 }
+
+//Draw food
+
+function drawFood(x, y) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+    ctx.fillStyle = 'black';
+    ctx.strokeRect(x * snakeW, y * snakeH, snakeW, snakeH)
+}
+
+function drawDefaultSnake() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < snake.length; i++) {
+        var X = snake[i].x;
+        var Y = snake[i].y;
+        drawSnake(X, Y)
+    }
+    //Snake Head
+    drawFood(food.x, food.y)
+    var snakeX = snake[0].x;
+    var snakeY = snake[0].y;
+    // console.log(snakeX,snakeY)
+
+    if (snakeX < 0 || snakeY < 0 || snakeX >= canvas.width / snakeW || snakeY >= canvas.height / snakeH) {
+        // alert("game over!")
+        ctx.font = "40px arial";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center"
+        ctx.fillText("Game Over", 250, 250)
+        ctx.font = "14px arial";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center"
+        ctx.fillText("press enter to restart", 250, 280)
+        clearInterval(intervalVar);
+    }
+
+    if (dir === "right") {
+        snakeX++
+    } else if (dir === "left") {
+        snakeX--
+    } else if (dir === "up") {
+        snakeY--
+    } else if (dir === "down") {
+        snakeY++
+    }
+    if (snakeX == food.x && snakeY == food.y) {
+        food = {
+            x: Math.round(Math.random() * (canvas.width / snakeW - 1) + 1),
+            y: Math.round(Math.random() * (canvas.height / snakeH - 1) + 1)
+        }
+        newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+    } else {
+        snake.pop();
+        newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+    }
+    for (let i = snake.length - 2; i >= 0; --i) {
+        if (snake[i].x == newHead.x && snake[i].y == newHead.y) {
+            // alert("collide!")
+            console.log("collide self body")
+            ctx.font = "40px arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center"
+            ctx.fillText("Game Over", 250, 250)
+            ctx.font = "14px arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center"
+            ctx.fillText("press enter to restart", 250, 280)
+            clearInterval(intervalVar);
+
+        }
+    }
+    snake.unshift(newHead);
+}
+
+
+// function demoX(x,y){
+// 	ctx.fillStyle = "red";
+//     ctx.fillRect(x * snakeW, y * snakeH, canvas.width, snakeH);
+// }
+// for(var i=0;i<=50;i++){
+// 	i++
+// 	demoX(0,i)
+// }
+// function demoY(x,y){
+// 	ctx.fillStyle = "red";
+//     ctx.fillRect(x * snakeW, y * snakeH, 10,canvas.height);
+// }
+// for(var i=0;i<=50;i++){
+// 	i++
+// 	demoY(i,0)
+// }
--- js/snake.js
+++ js/snake.js
@@ -1,95 +1,157 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var snakeW = 10;
-var snakeH = 10;
-var dir = "right";
+var gameData = {};
+gameData.flag = true;
+// var gameData.snakeW = 10;
+// var gameData.snakeH = 10;
+
+// let flag = true;
+var intervalVar;
+gameData.newHead;
+let foodInitalized = false;
+gameData.dir = "right";
+// var len = 4;
+// var snake = [];
+function initFood() {
+    gameData.food = {
+        x: Math.round(Math.random() * (canvas.width / gameData.snakeW - 1) + 1),
+        y: Math.round(Math.random() * (canvas.height / gameData.snakeH - 1) + 1)
+    }
+    foodInitalized = true;
+}
+
+
+function init() {
+    gameData.snake = []
+    gameData.len = 4;
+    gameData.snakeH = 10;
+    gameData.snakeW = 10;
+}
 
 function drawSnake(x, y) {
     ctx.fillStyle = "white";
-    ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+    ctx.fillRect(x * gameData.snakeW, y * gameData.snakeH, gameData.snakeW, gameData.snakeH);
     ctx.fillStyle = 'black';
-    ctx.strokeRect(x * snakeW, y * snakeH, snakeW, snakeH)
+    ctx.strokeRect(x * gameData.snakeW, y * gameData.snakeH, gameData.snakeW, gameData.snakeH)
 }
-//CreateSanke
-var len = 4;
 
-var snake = [];
-
-for (let i = len - 1; i >= 0; i--) {
-    snake.push({ x: i, y: 0 })
-}
-//control direction
+//Direction and controls
 document.addEventListener("keydown", dirControl)
 
 function dirControl(e) {
     console.log(e.key)
-    if (e.key == "ArrowRight" && dir != "left") {
-        dir = "right";
-    } else if (e.key === "ArrowLeft" && dir != "right") {
-        dir = "left"
-    } else if (e.key === "ArrowUp" && dir != "down") {
-        dir = "up"
-    } else if (e.key === "ArrowDown" && dir != "up") {
-        dir = "down"
+    if (e.which == 32) {
+        if (gameData.flag) {
+            console.log("Start the game")
+            intervalVar = setInterval(drawDefaultSnake, 150);
+            gameData.flag = false;
+        } else {
+            console.log("Pause the game")
+            clearInterval(intervalVar);
+            gameData.flag = true;
+        }
     }
-}
-var food = {
-    x: Math.round(Math.random() * (canvas.width / snakeW-1) + 1),
-    y: Math.round(Math.random() * (canvas.height / snakeH-1) + 1)
+    if (e.which == 13) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        gameData.flag = true;
+    }
+    if (e.key == "ArrowRight" && gameData.dir != "left") {
+        gameData.dir = "right";
+    } else if (e.key === "ArrowLeft" && gameData.dir != "right") {
+        gameData.dir = "left"
+    } else if (e.key === "ArrowUp" && gameData.dir != "down") {
+        gameData.dir = "up"
+    } else if (e.key === "ArrowDown" && gameData.dir != "up") {
+        gameData.dir = "down"
+    }
 }
 
 //Draw food
 
 function drawFood(x, y) {
     ctx.fillStyle = "red";
-    ctx.fillRect(x * snakeW, y * snakeH, snakeW, snakeH);
+    ctx.fillRect(x * gameData.snakeW, y * gameData.snakeH, gameData.snakeW, gameData.snakeH);
     ctx.fillStyle = 'black';
-    ctx.strokeRect(x * snakeW, y * snakeH, snakeW, snakeH)
+    ctx.strokeRect(x * gameData.snakeW, y * gameData.snakeH, gameData.snakeW, gameData.snakeH)
 }
 
 function drawDefaultSnake() {
+    init();
+    if(foodInitalized){
+        // console.log("Food Initialized already")
+    } else {
+        initFood();
+    }
+    console.log("gameData ",gameData)
+    for (let i = gameData.len - 1; i >= 0; i--) {
+        gameData.snake.push({ x: i, y: 0 })
+    }
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    for (let i = 0; i < snake.length; i++) {
-        var X = snake[i].x;
-        var Y = snake[i].y;
+    for (let i = 0; i < gameData.snake.length; i++) {
+        var X = gameData.snake[i].x;
+        var Y = gameData.snake[i].y;
         drawSnake(X, Y)
     }
     //Snake Head
-    drawFood(food.x, food.y)
-    var snakeX = snake[0].x;
-    var snakeY = snake[0].y;
+    drawFood(gameData.food.x, gameData.food.y)
+    var snakeX = gameData.snake[0].x;
+    var snakeY = gameData.snake[0].y;
+    // console.log(snakeX,snakeY)
 
-    if (snakeX < 0 || snakeY < 0 || snakeX >= canvas.width / snakeW || snakeY >= canvas.height / snakeH) {
-        alert("game over!")
+    if (snakeX < 0 || snakeY < 0 || snakeX >= canvas.width / gameData.snakeW || snakeY >= canvas.height / gameData.snakeH) {
+        // alert("game over!")
+        ctx.font = "40px arial";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center"
+        ctx.fillText("Game Over", 250, 250)
+        ctx.font = "14px arial";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center"
+        ctx.fillText("press enter to restart", 250, 280)
+        clearInterval(intervalVar);
     }
 
-    if (dir === "right") {
+    if (gameData.dir === "right") {
+        console.log('')
         snakeX++
-    } else if (dir === "left") {
+    } else if (gameData.dir === "left") {
         snakeX--
-    } else if (dir === "up") {
+    } else if (gameData.dir === "up") {
         snakeY--
-    } else if (dir === "down") {
+    } else if (gameData.dir === "down") {
         snakeY++
     }
-
-    if (snakeX == food.x && snakeY == food.y) {
-        food = {
-            x: Math.round(Math.random() * (canvas.width / snakeW-1) + 1),
-            y: Math.round(Math.random() * (canvas.height / snakeH-1) + 1)
+    if (snakeX == gameData.food.x && snakeY == gameData.food.y) {
+        gameData.food = {
+            x: Math.round(Math.random() * (canvas.width / gameData.snakeW - 1) + 1),
+            y: Math.round(Math.random() * (canvas.height / gameData.snakeH - 1) + 1)
         }
-        var newHead = {
+        gameData.newHead = {
             x: snakeX,
             y: snakeY
         }
     } else {
-        snake.pop();
-        var newHead = {
+        gameData.snake.pop();
+        gameData.newHead = {
             x: snakeX,
             y: snakeY
         }
     }
+    for (let i = gameData.snake.length - 2; i >= 0; --i) {
+        if (gameData.snake[i].x == gameData.newHead.x && gameData.snake[i].y == gameData.newHead.y) {
+            // alert("collide!")
+            console.log("collide self body")
+            ctx.font = "40px arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center"
+            ctx.fillText("Game Over", 250, 250)
+            ctx.font = "14px arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center"
+            ctx.fillText("press enter to restart", 250, 280)
+            clearInterval(intervalVar);
 
-    snake.unshift(newHead);
+        }
+    }
+    gameData.snake.unshift(gameData.newHead);
 }
-setInterval(drawDefaultSnake, 100);
